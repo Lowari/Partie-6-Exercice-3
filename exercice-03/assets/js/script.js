31e5400b49773555d0b41fd86704af23ba0ef20d
Lowari <66225181+Lowari@users.noreply.github.com>
2020-11-10 20:23:09 +0100

    Modification de structure et de syntaxe avec fonction fléhée

--- exercice-03/assets/js/script.js
+++ exercice-03/assets/js/script.js
@@ -1,12 +1,25 @@
 let languages = ['html', 'css', 'javascript', 'php', 'mysql', 'c++','ruby','python'];
-const languagesList = document.createElement('ul');
 
-languagesList.setAttribute('class', 'list-group text-center container col-4 mt-5');
-document.body.appendChild(languagesList);
+let table = document.createElement('table');
+let thead = document.createElement('thead');
+let tr = document.createElement('tr');
+let th = document.createElement('th');
+let tbody = document.createElement('tbody');
 
-languages.forEach(function(language) {
-    let itemLanguagesList = document.createElement('li');
-    itemLanguagesList.setAttribute('class', 'list-group-item');
-    itemLanguagesList.textContent = language;
-    languagesList.appendChild(itemLanguagesList);
+table.classList.add('table', 'text-center', 'col-2', 'container', 'mt-5', 'table-bordered');
+th.textContent = 'Languages Web';
+th.classList.add('text-center', 'col');
+
+document.body.appendChild(table);
+table.appendChild(thead);
+thead.appendChild(tr);
+tr.appendChild(th);
+table.appendChild(tbody);
+
+languages.forEach(element => {
+    let tr = document.createElement('tr');
+    let td = document.createElement('td');
+    td.textContent = element;
+    tbody.appendChild(tr);
+    tr.appendChild(td);
 });
